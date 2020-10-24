33707c9f3dcab59e38c21683ac79fb9f38bf4b62
Rafael Chaves <rafaelvchaves@gmail.com>
2020-10-24 15:50:18 -0400

    Added variant for standard tetrominoes

--- tetromino.ml
+++ tetromino.ml
@@ -1,3 +1,12 @@
+type tetromino_type =
+  | I_block
+  | L_block
+  | J_block
+  | O_block
+  | S_block
+  | T_block
+  | Z_block
+
 type t = {
   composition : (int * int) list;
 }
@@ -30,11 +39,18 @@ let z_block = {
   composition = [(0, 0); (1, 0); (1, 1); (2, 1)];
 }
 
+let create_tetromino = function
+  | I_block -> i_block
+  | L_block -> l_block
+  | J_block -> j_block
+  | O_block -> o_block
+  | S_block -> s_block
+  | T_block -> t_block
+  | Z_block -> z_block
+
 let generate_list () = 
   failwith "Unimplemented"
 
 let get_width tetromino = 
   failwith "Unimplemented"
-
-let get_height tetromino = 
-  failwith "Unimplemented"
+  
--- tetromino.mli
+++ tetromino.mli
@@ -1,14 +1,31 @@
-(** The type [tetromino] represents a tetris piece. *)
+(** The type [tetromino_type] represents the type of tetromino. Each type
+    has a different block composition. *)
+type tetromino_type =
+  | I_block
+  | L_block
+  | J_block
+  | O_block
+  | S_block
+  | T_block
+  | Z_block
+
+(** The type [t] represents a tetris piece: a composition of four squares
+    connected orthogonally. *)
 type t
+
+(** [create_tetromino t_type] is the tetromino given by the tetromino type
+    [t_type]. *)
+val create_tetromino : tetromino_type -> t
+
+(** [rotate tetromino] is a new tetromino after [tetromino] is rotated 90
+    degrees clockwise. It will shift the tetromino after rotation in order
+    to keep it within the same boundary. *)
+val rotate : t -> t
 
 (** [generate_list ()] is a list containing one of each of the seven
     tetrominoes in a random order. *)
 val generate_list : unit -> t list
 
-(** [get_width tetromino] is the number of blocks that [tetromino] takes up
-    in the x direction. *)
+(** [get_width tetromino] is the width/height of the square that encloses
+    [tetromino]. *)
 val get_width : t -> int
-
-(** [get_height tetromino] is the number of blocks that [tetromino] takes up
-    in the y direction. *)
-val get_height : t -> int
