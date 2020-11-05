bc79f770f9800bd33e34cbb99331c6d14b008a7b
davidshu241999 <davidshu24@gmail.com>
2020-11-05 11:17:39 -0800

    changed the start () to include State module

--- main.ml
+++ main.ml
@@ -9,12 +9,11 @@ let start () =
   ignore (read_key () : char);
   clear_graph ();
   (* This what we want to use but have not implemented Tetromino.generate_list
-     so it fails with Failure("Unimplemented") right now
-     let init = State.initialize () in
-     Display.draw_grid (State.get_grid init); 
-     Right now we hard code the array to make the list. 
-  *) 
-  Display.draw_grid (Array.make_matrix 10 20 0);
+     so it fails with Failure("Unimplemented") right now *)
+  let init = State.initialize () in
+  Display.draw_grid (State.get_grid init); 
+  (*Right now we hard code the array to make the list. *)
+  (*Display.draw_grid (Array.make_matrix 10 20 0);*)
   while true do
     let st = wait_next_event [ Mouse_motion; Button_down; Key_pressed ] in
     synchronize ();
