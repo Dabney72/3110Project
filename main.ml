fb66bb2d45b11256bcef44ad79cc468825362c8b
davidshu241999 <davidshu24@gmail.com>
2020-11-10 00:05:45 -0800

    Added a time interval just for testing.

--- main.ml
+++ main.ml
@@ -2,6 +2,7 @@ open Graphics
 open Display
 open State
 open Tetromino
+open Unix 
 
 let start () =
   open_graph "";
@@ -12,7 +13,16 @@ let start () =
   let init = State.initialize () in
   spawn_tetromino (Tetromino.init_tetromino J_block) init;
   drop init;
-  draw_game_screen init;
+  draw_game_screen init;  
+  (* Used Unix.sleep for the interval. I think it is pretty straightfoward that
+     Unix.sleep takes in an int, which is represented in seconds. times has to
+     be inputted manually so I guess put the largest number Ocaml can take if we 
+     are to make this work *)
+  let rec print_hello times =
+    Unix.sleep 3;
+    if times <> 0 then (print_endline "Hello World!"; print_hello (times-1)) 
+    else print_endline "Hello World"; 
+  in print_hello 10;
   while true do
     let st = wait_next_event [ Mouse_motion; Button_down; Key_pressed ] in
     synchronize ();
