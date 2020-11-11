98ce58e0a48f97c3f1f62387bdfb56fd18243b63
Dan <dabtn72@gmail.com>
2020-11-10 23:02:18 -0500

    Added player input to main

--- main.ml
+++ main.ml
@@ -1,39 +1,48 @@
 open Graphics
 open Display
-open State
 open Tetromino
+open State
 open Unix 
 
-let start () =
-  open_graph "";
+(** [read_input state char] updates game state [state] based on the character
+    [char] is or does nothing if [char] is not one of the valid inputs. 
+    Valid inputs are w, a, s, d, and space. *)
+let read_input state =
+  function
+  | 'w' -> rotate state
+  | 'a' -> move_left state
+  | 'd' -> move_right state
+  | 's' -> drop state
+  | ' ' -> hold state
+  | _ -> ()
+
+(** [main ()] runs the tetris game. *)
+let rec main () =
+  (* Initialize game variables and game state and wait for any button press to 
+     start game. *)
+  let counter = ref 0 in
+  let diff = ref 5 in
   draw_start_screen ();
-  ignore (read_key () : char);
-  clear_graph ();
+  ignore (read_key ());
   let state = initialize () in
-  draw_game_screen state;  
-  (* Used Unix.sleep for the interval. I think it is pretty straightfoward that
-     Unix.sleep takes in an int, which is represented in seconds. times has to
-     be inputted manually so I guess put the largest number Ocaml can take if we 
-     are to make this work *)
-  (* let rec game () =
-    Unix.sleepf 0.75;
-    spawn_next state; move_left state; drop state; draw_game_screen state; game ()
-  in game (); *)
-
-  (* I changed initialize () to already spawn the first tetromino, and then
-    the state will automatically spawn a new one when the current one stops
-    falling, so we won't need to explicity call spawn anywhere in main.ml.Bigarray
-    
-    I also added a new field to the state record to determine if the game is over
-    or not, so this will be the condition for the while loop. *)
+  draw_game_screen state;
+  (* Main game loop that runs until game over. *)  
   while (not (game_over state)) do
-    Unix.sleepf 0.75;
-    drop state; draw_game_screen state;
+    sleepf 0.1;
+    (* Checks if there is an input from the player. *)
+    let () = if key_pressed () then read_input state (read_key ()) else () in
+    draw_game_screen state;
+    (* Increments counter until it is greater than diff to cause a game update. 
+       In the future diff can be changed to speedup the game. *)
+    counter := !counter + 1;
+    let () = if !counter > !diff then fall state else () in
+    let () = if !counter > !diff then draw_game_screen state else () in
+    let () = if !counter > !diff then counter := 0 else () in
+    ()
   done;
-  while true do
-    let st = wait_next_event [ Mouse_motion; Button_down; Key_pressed ] in
-    synchronize ();
-    if st.keypressed then close_graph ();
-  done
+  (* Game over where any button press will restart main. *)
+  draw_game_over_screen (get_score state);
+  ignore (read_key ());
+  main ()
 
-let () = start ()
+let () = main ()
