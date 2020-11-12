37afeb05439ec1cc803b584b57fa2feccd407054
Lenhard Thomas <thomaslenhard3@gmail.com>
2020-11-12 18:52:47 -0500

    Implement collision detection for rotate

--- state.ml
+++ state.ml
@@ -244,10 +244,30 @@ let move_left st =
 let move_right st =
   if not (collision_right st) then move st Right
 
+(** [valid_position st] takes in the state [st] and returns the block that
+    is falling. 
+    [Failure "No block falling"] if st.falling_block is None  *)
+let valid_position st old_pos r c =
+  if r < 0 || c < 0 then false 
+  else if r >= grid_height st || c >= grid_width st then false
+  else st.grid.(r).(c) = 0  || List.mem(r, c) old_pos 
+
 let rotate st =
   let falling = get_falling st in
-  place_block st falling.pos falling.block 0;
-  place_block st falling.pos (rotate falling.block) 1
+  let rotate_comp = get_comp (rotate falling.block) in
+  let add_pos acc (y, x) = (fst falling.pos + x, snd falling.pos + y) :: acc in
+  let falling_positions = List.fold_left add_pos [] (get_comp falling.block) in 
+  let valid_placement (y, x) =
+    match st.falling_block with 
+    | None -> failwith "No block falling"
+    | Some {block; pos = (r, c)} -> 
+      valid_position st falling_positions (r + x) (c + y) in
+  let valid_rotate = List.for_all valid_placement rotate_comp in
+  if valid_rotate
+  then begin
+    place_block st falling.pos falling.block 0;
+    place_block st falling.pos (rotate falling.block) 1
+  end
 
 let drop ?auto_respawn:(auto = true) st =
   while not (collision_under st) do
